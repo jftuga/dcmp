4f97e5f7a5b089050347fcf1d261dab2695484d6
John Taylor <johntaylor@gmail.com>
2017-12-07 20:52:11 -0500

    added MIT License

--- dcmp.py
+++ dcmp.py
@@ -6,6 +6,11 @@
 
 # Compare files within two directory trees for equivalency
 
+# MIT License; Copyright (c) 2017 John Taylor
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import argparse
 import io
 import multiprocessing
@@ -34,7 +39,7 @@ class dir_compare():
 	"""Compare files within two directory trees for equivalency
 	"""
 
-	class_version = "1.03"
+	class_version = "1.04"
 
 	# output date format
 	date_time_fmt = "%m/%d/%y %H:%M:%S"
@@ -58,7 +63,7 @@ class dir_compare():
 	mgr = MyManager()
 	file_stats = None  # this is redefined in __init__()
 
-	# when -p is used, save the files that are different, via a list of tuples (similar to all_twins)
+	# when --pgm is used, save the files that are different, via a list of tuples (similar to all_twins)
 	output_files = []
 	# when --stats is used, keep track of program run time
 	time_start = 0
@@ -175,7 +180,6 @@ class dir_compare():
 		mutuallyexclusive_opts.add_argument("--one", "-1", action="store_true", help="only show files in dname1")
 		mutuallyexclusive_opts.add_argument("--two", "-2", action="store_true", help="only show files in dname2")
 		mutuallyexclusive_opts.add_argument('--version', action='version', version='version %s' % (self.class_version))
-
 
 		output_opts = parser.add_argument_group(title="Output Arguments",description=None)
 		output_opts.add_argument("--pgm", action=None, help="output diff commands, by using PGM as your comparision program")
